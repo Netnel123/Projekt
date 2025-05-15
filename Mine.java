c3615443459fec6e2be62b1ad57baa509a4b4974
ASUS <netanelekshtein@gmail.com>
2025-05-15 19:49:48 +0300

    no more riting

--- Mine.java
+++ Mine.java
@@ -2,5 +2,9 @@ public class Mine {
     public static void main(String[] args) {
         System.out.println("aloo");
         System.out.println("oloo world");
+        print("no more riting");
+    }
+    public static void print(String writing){
+        System.out.println(writing);
     }
 }
